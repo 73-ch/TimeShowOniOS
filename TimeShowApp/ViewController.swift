911aafaf62c14e96e8a71d259a0fa4756ed23de9
nami634 <cigarbox.nami@gmail.com>
2018-09-24 15:48:19 +0900

    Initial Commit

---
+++ TimeShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeShowApp
+//
+//  Created by nami on 2018/09/24.
+//  Copyright © 2018年 Kousaku Namikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
